f64a5347cbd33757f2ffff36d79320adc3538074
Alex White <alex_p_white@yahoo.co.uk>
2015-04-15 21:23:47 +0100

    added website for api dev

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+
+		<meta charset="utf-8" />
+
+		<!-- User defined head content such as meta tags and encoding options -->
+		
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="generator" content="RapidWeaver" />
+		
+
+		<!-- User defined head content -->
+		<?php date_default_timezone_set('Europe/London'); ?>
+
+		<!-- Meta tags -->
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+		<title>Positive Touch - Charity Website | Positive Touch</title>
+
+		<!-- Load some fonts from Google's Font service -->
+		<link href='http://fonts.googleapis.com/css?family=Open Sans:400,300,800|Noto Serif|Arvo' rel='stylesheet' type='text/css'>
+
+		<!-- CSS stylesheets reset -->
+	  <link rel="stylesheet" href="rw_common/themes/tesla/css/normalize.css" />
+
+		<!-- CSS for the Foundation framework's CSS that handles the responsive columnized layout -->
+	  <link rel="stylesheet" href="rw_common/themes/tesla/css/foundation.css" />
+
+	  <!-- Main Stylesheet -->
+		<link rel="stylesheet" type="text/css" media="screen" href="rw_common/themes/tesla/styles.css"  />
+
+	  <!-- Loads Font Awesome v4.0.3 CSS from CDN -->
+	  <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	
+		<!-- RapidWeaver Color Picker Stylesheet -->
+		<link rel="stylesheet" type="text/css" media="screen" href="rw_common/themes/tesla/colourtag-theme-default.css" />  
+
+	  <!-- Theme specific media queries -->
+		<link rel="stylesheet" href="rw_common/themes/tesla/css/media_queries.css" />
+
+		<!-- Base RapidWeaver javascript -->
+		<script type="text/javascript" src="rw_common/themes/tesla/javascript.js"></script>
+
+		<!-- jQuery 1.8 is included in the theme internally -->
+	  <script src="rw_common/themes/tesla/js/jquery.min.js"></script>
+
+	  <!-- Elixir specific javascript, along with jQuery Easing and a few other elements -->
+	  <script src="rw_common/themes/tesla/js/elixir.js"></script>
+
+		<!-- Style variations -->
+		<link rel="stylesheet" type="text/css" media="screen" href="rw_common/themes/tesla/css/toggles/title_shadow.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="rw_common/themes/tesla/css/toggles/slogan_shadow.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="rw_common/themes/tesla/css/width/width_1060px.css" />
+		<link rel="stylesheet" type="text/css" media="screen" href="rw_common/themes/tesla/css/banner/banner_1.css" />
+		<script type="text/javascript" src="rw_common/themes/tesla/js/sidebar/sidebar_hidden.js"></script>
+		
+
+		<!-- User defined styles -->
+		<style type="text/css" media="all">#slideshow img {padding-top:10px;}
+.hidden {display:none;}
+.visible {display:block;}
+.floatcal {float:left;overflow:visible;}</style>
+
+		<!-- User defined javascript -->
+		<script type="text/javascript">
+			 $mirage(document).ready(function () {
+  $mirage(function () {
+    $mirage('#slideshow img').each(function () {
+      $mirage('img').attr('style','max-width:300px;');
+    });
+  });
+  $mirage(function () {
+    $mirage('#slideshow').removeClass("hidden").addClass("visible");
+  });
+}); 
+		</script>
+
+		<!-- Plugin injected code -->
+		
+
+	
+	</head>
+	
+	<!-- This page was created with RapidWeaver from Realmac Software. http://www.realmacsoftware.com -->
+
+	<body>
+
+			<header role="banner">
+
+				<!-- Site Logo -->
+				<div id="logo" data-0="opacity: 1;" data-top-bottom="opacity: 0;" data-anchor-target="#logo">
+			  	<img src="rw_common/images/positivetouchlogo.png" width="259" height="240" alt="Site logo"/>  
+				</div>
+				
+				<div id="title_wrapper">
+						<!-- Site Title -->
+						<h1 id="site_title" data-0="opacity: 1; top:0px;" data-600="opacity: 0; top: 80px;" data-anchor-target="#site_title">
+							Positive Touch
+						</h1>
+
+						<!-- Site Slogan -->
+						<h2 id="site_slogan" data-0="opacity: 1; top:0px;" data-600="opacity: 0; top: 80px;" data-anchor-target="#site_slogan">
+							Supporting Special Care Babies & their Families at RSCH
+						</h2>
+
+						<!-- Scroll down button -->
+						<div id="scroll_down_button" data-0="opacity: 1; top:0px;" data-400="opacity: 0; top: 100px;" data-anchor-target="#scroll_down_button">
+							<i class="fa fa-angle-down"></i>
+						</div>
+				</div>
+				
+				<!-- Top level navigation -->
+				<div id="navigation_bar">
+					<div class="row site_width">
+						<div class="large-12 columns">
+							<nav id="top_navigation"><ul><li><a href="./" rel="" id="current">Home</a></li></ul></nav>
+						</div>
+					</div>
+				</div>
+
+			</header>
+
+		<!-- Sub-navigation -->
+		<div id="sub_navigation_bar">
+			<div class="row site_width">
+				<div class="large-12 columns">
+					<nav id="sub_navigation"><ul></ul></nav>
+				</div>
+			</div>
+		</div>
+
+		<!-- Mobile Navigation -->
+		<div id="mobile_navigation_toggle">
+			<i id="mobile_navigation_toggle_icon" class="fa fa-bars"></i>
+		</div>
+		<nav id="mobile_navigation">
+			<ul><li><a href="./" rel="" id="current">Home</a></li></ul>
+		</nav>
+
+		<!-- Main Content area and sidebar -->
+		<div class="row site_width" id="container">
+			<section id="content"class="large-8 columns">
+				<div class="image-left"><img class="imageStyle" alt="positivetouchlogo" src="files/positivetouchlogo.png" width="259" height="240" /></div><span style="font:12px &#39;Lucida Grande&#39;, LucidaGrande, Verdana, sans-serif; ">Positive Touch was formed to support the RSCH SCBU Babies and their families.<br /><br />Babies and their families who have spent time at SCBU can use this page as a support network for all their issues, woes and mile stones that only SCBU graduates and their parents will understand </span><span style="font:12px Courier, mono; "><i class="fa fa-smile-o"></i><br /></span><span style="font:12px &#39;Lucida Grande&#39;, LucidaGrande, Verdana, sans-serif; "><br />So please feel free to post any questions that you feel need answering. You will be surprised how many families have gone or are going through the same thing you are and who will be more than happy to help!<br /><br />Keep up-to-date with us on </span><span style="font:12px &#39;Lucida Grande&#39;, LucidaGrande, Verdana, sans-serif; "><a href="https://www.facebook.com/Positivetouchgroup" rel="external" title="Positive Touch Facebook Page">Facebook </a></span><span style="font:12px Courier, mono; "><a href="https://www.facebook.com/Positivetouchgroup" rel="external" title="Positive Touch Facebook Page"><i class="fa fa-facebook-square"></i></a></span><span style="font:12px Courier, mono; "><br /></span>
+			</section>
+			<aside id="sidebar" class="large-4 columns">
+				<h4 id="sidebar_title"></h4>
+				<div id="sidebar_content"></div>
+				<div id="archives">
+					
+				</div>
+			</aside>
+		</div>
+
+		<!-- Footer -->
+		<footer class="row site_width">
+			<div id="footer_content" class="large-12 columns">
+				<div id="breadcrumb_container">
+					<i class="fa fa-folder-open-o"></i> <span id="breadcrumb"><ul><li><a href="./">Home</a>&nbsp;>&nbsp;</li></ul></span>
+				</div>
+				&copy; <?php echo date("Y") ?>
+			</div>
+		</footer>
+
+		<div id="scroll_up_button"><i class="fa fa-angle-up"></i></div>
+
+		<!-- Handles loading Skrollr, which helps in animating portions of the header area. -->
+		<!-- We check to see if the user is on an mobile device or not, and only serve up -->
+		<!-- the animations on non-mobile devices -->
+		<script>
+			$elixir(window).load(function() {
+			  if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+			      skrollr.init({
+			          forceHeight: false
+			      });
+			  }
+			});
+		</script>
+
+	</body>
+
+</html>
